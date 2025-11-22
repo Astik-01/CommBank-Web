344cc247c49f6e97768b97468436579c734a7dc5
Astik Patil <166976517+Astik-01@users.noreply.github.com>
2025-11-22 15:49:41 +0530

    Implement emoji icon support in GoalManager
    Added emoji icon support to GoalManager component, including state management for icon selection and an emoji picker UI.

--- src/ui/features/goalmanager/GoalManager.tsx
+++ src/ui/features/goalmanager/GoalManager.tsx
@@ -1,5 +1,5 @@
 import { faCalendarAlt } from '@fortawesome/free-regular-svg-icons'
-import { faDollarSign, IconDefinition } from '@fortawesome/free-solid-svg-icons'
+import { faDollarSign } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { MaterialUiPickersDate } from '@material-ui/pickers/typings/date'
 import 'date-fns'
@@ -12,38 +12,46 @@ import { useAppDispatch, useAppSelector } from '../../../store/hooks'
 import DatePicker from '../../components/DatePicker'
 import { Theme } from '../../components/Theme'
 
+// NEW IMPORTS FOR ICON SUPPORT
+import { Picker } from 'emoji-mart'
+import 'emoji-mart/css/emoji-mart.css'
+
 type Props = { goal: Goal }
+
 export function GoalManager(props: Props) {
   const dispatch = useAppDispatch()
-
   const goal = useAppSelector(selectGoalsMap)[props.goal.id]
 
   const [name, setName] = useState<string | null>(null)
   const [targetDate, setTargetDate] = useState<Date | null>(null)
   const [targetAmount, setTargetAmount] = useState<number | null>(null)
 
+  // NEW STATE FOR ICON
+  const [icon, setIcon] = useState<string | null>(goal.icon ?? null)
+  const [showPicker, setShowPicker] = useState(false)
+
   useEffect(() => {
     setName(props.goal.name)
     setTargetDate(props.goal.targetDate)
     setTargetAmount(props.goal.targetAmount)
+    setIcon(props.goal.icon ?? null)
   }, [
     props.goal.id,
     props.goal.name,
     props.goal.targetDate,
     props.goal.targetAmount,
+    props.goal.icon,
   ])
 
   useEffect(() => {
     setName(goal.name)
-  }, [goal.name])
+    setIcon(goal.icon ?? null)
+  }, [goal.name, goal.icon])
 
   const updateNameOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const nextName = event.target.value
     setName(nextName)
-    const updatedGoal: Goal = {
-      ...props.goal,
-      name: nextName,
-    }
+    const updatedGoal: Goal = { ...props.goal, name: nextName, icon: icon ?? undefined }
     dispatch(updateGoalRedux(updatedGoal))
     updateGoalApi(props.goal.id, updatedGoal)
   }
@@ -56,6 +64,7 @@ export function GoalManager(props: Props) {
       name: name ?? props.goal.name,
       targetDate: targetDate ?? props.goal.targetDate,
       targetAmount: nextTargetAmount,
+      icon: icon ?? undefined,
     }
     dispatch(updateGoalRedux(updatedGoal))
     updateGoalApi(props.goal.id, updatedGoal)
@@ -69,16 +78,67 @@ export function GoalManager(props: Props) {
         name: name ?? props.goal.name,
         targetDate: date ?? props.goal.targetDate,
         targetAmount: targetAmount ?? props.goal.targetAmount,
+        icon: icon ?? undefined,
       }
       dispatch(updateGoalRedux(updatedGoal))
       updateGoalApi(props.goal.id, updatedGoal)
     }
   }
 
+  // ----------------------
+  // UPDATE ICON WHEN USER SELECTS EMOJI
+  // ----------------------
+  const handleEmojiSelect = (emoji: any) => {
+    const selected = emoji.native
+    setIcon(selected)
+    setShowPicker(false)
+
+    const updatedGoal: Goal = {
+      ...props.goal,
+      name: name ?? props.goal.name,
+      targetDate: targetDate ?? props.goal.targetDate,
+      targetAmount: targetAmount ?? props.goal.targetAmount,
+      icon: selected,
+    }
+
+    dispatch(updateGoalRedux(updatedGoal))
+    updateGoalApi(props.goal.id, updatedGoal)
+  }
+
   return (
     <GoalManagerContainer>
+
+      {/* GOAL TITLE */}
       <NameInput value={name ?? ''} onChange={updateNameOnChange} />
 
+      {/* ---------------- ICON PICKER UI ---------------- */}
+      <Group>
+        <Field name="Goal Icon" icon={faDollarSign} />
+        <Value>
+          <IconRow>
+            {/* Display chosen icon */}
+            {icon ? (
+              <IconDisplay>{icon}</IconDisplay>
+            ) : (
+              <NoIconText>No icon selected</NoIconText>
+            )}
+
+            {/* Button to pick emoji */}
+            <IconButton onClick={() => setShowPicker(!showPicker)}>
+              {icon ? 'Change Icon' : 'Add Icon'}
+            </IconButton>
+          </IconRow>
+
+          {/* Emoji Picker */}
+          {showPicker && (
+            <EmojiPickerWrapper>
+              <Picker onSelect={handleEmojiSelect} title="Pick an emoji" />
+            </EmojiPickerWrapper>
+          )}
+        </Value>
+      </Group>
+
+      {/* OTHER UI FIELDS */}
       <Group>
         <Field name="Target Date" icon={faCalendarAlt} />
         <Value>
@@ -110,12 +170,11 @@ export function GoalManager(props: Props) {
   )
 }
 
-type FieldProps = { name: string; icon: IconDefinition }
-type AddIconButtonContainerProps = { shouldShow: boolean }
-type GoalIconContainerProps = { shouldShow: boolean }
-type EmojiPickerContainerProps = { isOpen: boolean; hasIcon: boolean }
+// ------------------------------------
+// STYLED COMPONENTS BELOW
+// ------------------------------------
 
-const Field = (props: FieldProps) => (
+const Field = (props: { name: string; icon: any }) => (
   <FieldContainer>
     <FontAwesomeIcon icon={props.icon} size="2x" />
     <FieldName>{props.name}</FieldName>
@@ -125,13 +184,9 @@ const Field = (props: FieldProps) => (
 const GoalManagerContainer = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: flex-start;
   align-items: flex-start;
-  height: 100%;
   width: 100%;
-  position: relative;
 `
-
 const Group = styled.div`
   display: flex;
   flex-direction: row;
@@ -140,13 +195,11 @@ const Group = styled.div`
   margin-bottom: 1.25rem;
 `
 const NameInput = styled.input`
-  display: flex;
   background-color: transparent;
-  outline: none;
   border: none;
   font-size: 4rem;
   font-weight: bold;
-  color: ${({ theme }: { theme: Theme }) => theme.text};
+  outline: none;
 `
 
 const FieldName = styled.h1`
@@ -157,28 +210,53 @@ const FieldName = styled.h1`
 `
 const FieldContainer = styled.div`
   display: flex;
-  flex-direction: row;
   align-items: center;
   width: 20rem;
-
-  svg {
-    color: rgba(174, 174, 174, 1);
-  }
 `
+
 const StringValue = styled.h1`
   font-size: 1.8rem;
   font-weight: bold;
 `
+
 const StringInput = styled.input`
-  display: flex;
   background-color: transparent;
-  outline: none;
   border: none;
+  outline: none;
   font-size: 1.8rem;
   font-weight: bold;
-  color: ${({ theme }: { theme: Theme }) => theme.text};
 `
 
 const Value = styled.div`
   margin-left: 2rem;
+`
+
+// ---------------- ICON UI STYLES ----------------
+
+const IconRow = styled.div`
+  display: flex;
+  align-items: center;
+`
+
+const IconDisplay = styled.span`
+  font-size: 2.5rem;
+  margin-right: 1rem;
+`
+
+const NoIconText = styled.span`
+  font-size: 1.5rem;
+  color: gray;
+  margin-right: 1rem;
+`
+
+const IconButton = styled.button`
+  padding: 0.4rem 1rem;
+  font-size: 1rem;
+  border-radius: 0.5rem;
+  cursor: pointer;
+`
+
+const EmojiPickerWrapper = styled.div`
+  position: absolute;
+  z-index: 10;
 `
